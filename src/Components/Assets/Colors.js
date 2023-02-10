0da6266b09305db865ebd277798afa392c3bbf06
Rishabh Jain <rishabhjms99@gmail.com>
2023-02-10 16:38:30 +0530

    Pre-release V1

--- src/Components/Assets/Colors.js
+++
@@ -1,9 +0,0 @@
-export const colorPalette = {
-    "Dark green": "0a210f",
-    "Cal Poly green": "14591d",
-    "Tea green": "bfffbc",
-    "Uranian Blue": "acd2ed",
-    "White": "fff",
-    "Licorice brown": "170905",
-    "Smoky black": "080302"
-}
